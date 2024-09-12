8aed11276665f1856835171219f160a1101fd778
apurayy <apurayllpi@gmail.com>
2024-09-12 22:07:04 +0600

    pagination done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function user_list(){
-        return view('admin.user.user_list');
+        $data['getRecord'] = User::getRecordUser();
+        $users = User::paginate(2);
+        return view('admin.user.user_list', $data, compact('users'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,19 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    static public function getSingle($id)
+    {
+        return self::find($id);
+    }
+
+    static public function getRecordUser()
+    {
+        return self::select('users.*')
+            ->where('is_admin','=',0)
+            ->where('is_delete','=',0)
+            ->orderBy('users.id')
+            ->paginate(10);
+    }
+
 }
